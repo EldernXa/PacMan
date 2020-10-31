76723d66974d978443afa2d90ce4d65a6979e4c1
Anthony <anthony_pro1998@outlook.fr>
2020-10-31 12:48:55 +0100

    add sdk javafx for window and linux

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("PacMan");
+        primaryStage.show();
 
     }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
