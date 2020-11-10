7cdee177f80678366292174b539c1424c2549670
sykom1 <saber.tnifas@etu.univ-amu.fr>
2020-11-10 22:36:43 +0100

    ajout du bouton retour

--- src/GraphicsEngine/MenuParametres.java
+++ src/GraphicsEngine/MenuParametres.java
@@ -2,11 +2,16 @@ package GraphicsEngine;
 
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -15,13 +20,15 @@ import java.awt.*;
 public class MenuParametres {
     private Scene scene;
     private Scene sceneBack;
-    private Pane pane = new Pane();
+    private StackPane pane = new StackPane();
     private Slider volumeSlider = new Slider();
+    ImageViewSizePos revenir ;
 
 
     public MenuParametres(Stage stage,Scene sceneBack) {
         this.sceneBack = sceneBack;
-        scene = new Scene(pane, Screen.getPrimary().getVisualBounds().getWidth(),Screen.getPrimary().getVisualBounds().getHeight());
+        scene = new Scene(pane, Screen.getPrimary().getVisualBounds().getWidth()/2,Screen.getPrimary().getVisualBounds().getHeight()/2);
+
         /*ToggleGroup group = new ToggleGroup();
         RadioButton button1 = new RadioButton("Male");
         button1.setToggleGroup(group);
@@ -37,8 +44,35 @@ public class MenuParametres {
                 Musique.mediaPlayer.setVolume(volumeSlider.getValue()/100);
             }
         });
+        revenir  = new ImageViewSizePos("./data/Logos/return.png",50,50,new Coordinate(2,2));
 
-        pane.getChildren().add(volumeSlider);
+        pane.getChildren().addAll(volumeSlider,revenir.getImageView());
+
+        revenir.getImageView().setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+                stage.setScene(sceneBack);
+            }
+        });
+        revenir.getImageView().setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                revenir.setImageView("./data/Logos/returnhover.png");
+            }
+        });
+
+        revenir.getImageView().setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                revenir.setImageView("./data/Logos/return.png");
+
+            }
+        });
+        StackPane.setAlignment(revenir.getImageView(),Pos.TOP_LEFT);
+        volumeSlider.setMaxSize(100,100);
+        volumeSlider.setOrientation(Orientation.VERTICAL);
+        StackPane.setAlignment(volumeSlider, Pos.CENTER_RIGHT);
         stage.setScene(scene);
     }
 
