5a82b7ab0384bb82ace1616c754f986ee8c866d0
LaVolpe05 <loumi-05@hotmail.fr>
2020-11-12 18:55:38 +0100

    Création de la première carte Pac-Man , entièrement à la main (durée : +8 heures) | Recommendation : automatisé la génération et changé le principes de graphiques

--- src/GraphicsEngine/MenuChoixDifficulté.java
+++ src/GraphicsEngine/MenuChoixDifficulté.java
@@ -57,7 +57,7 @@ public class MenuChoixDifficulté {
                     Pane root = new Pane();//Creation groupe
                     root.setMinSize(0, 0);
                     Scene scene = new Scene(root,getScene().getWidth()/2,getScene().getHeight()/2);//Creation fenetre de taille 400 sur 400 pixels
-                    PacMan visualObject = new PacMan("./data/pacmanOuvert2.png", new Coordinate(0, 0), scene, root);
+                    PacMan visualObject = new PacMan("./data/pacmanOuvert2.png", new Coordinate(385, 20), scene, root);
                     root.getChildren().add(visualObject.getImageView());
                     ReadFileMapPacman readFileMapPacman = new ReadFileMapPacman(scene,root);
                     readFileMapPacman.decrypt();
