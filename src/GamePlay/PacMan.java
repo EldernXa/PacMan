8352bb79612cdc636050e4b1acea9c3f5ad3072d
sykom1 <saber.tnifas@etu.univ-amu.fr>
2020-11-17 09:38:32 +0100

    Début de la conclusion et création des attributs pour le pacman (qui reste encore théorique, et modifiable)

--- src/GamePlay/PacMan.java
+++ src/GamePlay/PacMan.java
@@ -1,6 +1,7 @@
 package GamePlay;
 
 import GraphicsEngine.ActionContinue;
+import GraphicsEngine.Conclusion;
 import GraphicsEngine.Coordinate;
 import GraphicsEngine.MouvingObject;
 import javafx.scene.Scene;
@@ -8,6 +9,10 @@ import javafx.scene.layout.Pane;
 
 public class PacMan extends MouvingObject {
 
+    private final int nbVies = 3;
+    private int nbVies_restantes = 3;
+    private int nbPoints = 0;
+    boolean canEatGhost = false;
     private float valueTps = (float)100;
 
     public PacMan(String path, Coordinate coordinate, Scene scene, Pane pane){
@@ -18,4 +23,33 @@ public class PacMan extends MouvingObject {
         addAction(new ActionContinue(getGameImage(), scene, "d", 'x', getGameImage().getValueMove(), valueTps, this));
     }
 
+    public PacMan(String path, Coordinate coordinate, Scene scene, Pane pane, int nbViesMax, int nbVies) {
+        super(path, coordinate, scene, pane);
+    }
+
+
+    public int getNbVies() {
+        return nbVies;
+    }
+
+    public int getNbVies_restantes() {
+        return nbVies_restantes;
+    }
+
+    public void manger(String item){
+        if(item.equals("boule")){
+            nbPoints++;
+        }
+        else if(item.equals("cerise")){
+            nbPoints+=2;
+            canEatGhost = true;
+
+        }
+        else if(item.equals("Fantome")){
+            nbPoints+=5;
+        }
+    }
+    public int getNbPoints() {
+        return nbPoints;
+    }
 }
