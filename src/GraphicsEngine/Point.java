236171c6f970b757c4f49880e622bc4f37b3af6f
Farouk Kedadi <farouk.kedadi@etu.uni-amu.fr>
2020-11-20 11:21:16 +0100

    Ajouts des points sur la map sauf sous pacman

---
+++ src/GraphicsEngine/Point.java
@@ -0,0 +1,10 @@
+package GraphicsEngine;
+
+import javafx.scene.Scene;
+
+public class Point extends UnmouvingObj{
+
+    public Point(Coordinate coordinate, Scene scene) {
+        super("./data/Point&Fruit/point.png", coordinate, scene);
+    }
+}
--- src/GraphicsEngine/UnmouvingObj.java
+++ src/GraphicsEngine/UnmouvingObj.java
@@ -1,12 +1,16 @@
 package GraphicsEngine;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+
+import java.io.File;
 
 public class UnmouvingObj extends VisualObject{
 
     public UnmouvingObj(String path, Coordinate coordinate, Scene scene){
         super(path, coordinate, scene);
+        super.getGameImage().setImage(new Image(new File(path).toURI().toString()));
     }
 
 }
