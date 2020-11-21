f3b50fd8fb7d2c789aca65b4e9eadc7952f4c345
Anthony <anthony_pro1998@outlook.fr>
2020-11-21 10:34:42 +0100

    fix the problem who occur when we hold a button.

--- src/GraphicsEngine/ActionContinue.java
+++ src/GraphicsEngine/ActionContinue.java
@@ -1,13 +1,14 @@
 package GraphicsEngine;
 
-//import ReadFile.ReadFileMapPacman;
-import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 
 public class ActionContinue extends Action{
@@ -16,9 +17,11 @@ public class ActionContinue extends Action{
     private Timeline timeline;
     private int indTimeline;
     private final MouvingObject mouvingObject;
+    private final LinkedList<KeyCode> listKeyEvent;
 
     public ActionContinue(GameImage image, Scene scene, String carac, double x, double y, int dir, String nameAction, float tps, MouvingObject mouvingObject){
         super(image, scene, carac, x, y, dir, nameAction, mouvingObject);
+        listKeyEvent = new LinkedList<>();
         this.mouvingObject = mouvingObject;
         this.tps = tps;
     }
@@ -71,6 +74,22 @@ public class ActionContinue extends Action{
         VisualObject.removeTimeline(indTimeline);
         VisualObject.startTimelineParallel();
         mouvingObject.setActionNext(null);
+    }
+
+    @Override
+    public void eventAppear(KeyEvent keyEvent, String carac){
+        if(!listKeyEvent.contains(keyEvent.getCode())){
+            listKeyEvent.push(keyEvent.getCode());
+            super.eventAppear(keyEvent, carac);
+        }
+    }
+
+    @Override
+    public void runEvent(Scene scene, String carac, int dir){
+        super.runEvent(scene, carac, dir);
+        scene.addEventHandler(KeyEvent.KEY_RELEASED, keyEvent -> {
+            listKeyEvent.remove(keyEvent.getCode());
+        });
     }
 
 }
