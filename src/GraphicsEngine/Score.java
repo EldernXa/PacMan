87c0152ff1c9b36cfebab3b30719fbe819068f24
Anthony <anthony_pro1998@outlook.fr>
2020-12-06 16:41:51 +0100

    généralisation de Score

--- src/GraphicsEngine/Score.java
+++ src/GraphicsEngine/Score.java
@@ -1,10 +1,37 @@
 package GraphicsEngine;
 
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public interface Score {
+import java.util.ArrayList;
 
-    void modifyRealTimeValue(VisualObject visualObject, Stage stage);
+public abstract class Score {
+    private final ArrayList<Node> listNode;
+    private final Stage stage;
+    private final HBox score;
+    public Score(Stage stage){
+        this.score = new HBox();
+        this.stage = stage;
+        this.listNode = new ArrayList<>();
+    }
+
+    public Stage getStage(){
+        return stage;
+    }
+
+    public void addNode(Node node){
+        listNode.add(node);
+    }
+
+    public void setHBox(){
+        score.getChildren().addAll(listNode);
+    }
+
+    public HBox getScore(){
+        return score;
+    }
+
+    public abstract void modifyRealTimeValue(VisualObject visualObject, Stage stage);
 
 }
